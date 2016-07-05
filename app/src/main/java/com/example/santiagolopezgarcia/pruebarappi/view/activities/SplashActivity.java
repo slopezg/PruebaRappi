d24fe3b7624e53e499a335901533a837495d7f94
Ronald Gallego Duque <ronaldgallegoduque@MacBook-Pro-de-Ronald.local>
2016-07-04 20:49:01 -0500

    image splash

--- app/src/main/java/com/example/santiagolopezgarcia/pruebarappi/view/activities/SplashActivity.java
+++ app/src/main/java/com/example/santiagolopezgarcia/pruebarappi/view/activities/SplashActivity.java
@@ -3,19 +3,31 @@ package com.example.santiagolopezgarcia.pruebarappi.view.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.santiagolopezgarcia.pruebarappi.R;
+
+import butterknife.InjectView;
 
 /**
  * Created by ronaldgallegoduque on 3/07/16.
  */
 public class SplashActivity extends Activity {
 
+    @InjectView(R.id.lyMain)
+    LinearLayout lyMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if(getResources().getBoolean(R.bool.isTablet)){
+            lyMain.setBackground(getResources().getDrawable(R.mipmap.ic_tablet_splash));
+        }else {
+            lyMain.setBackground(getResources().getDrawable(R.mipmap.ic_smatphone_splash));
+        }
 
         Thread timerThread = new Thread() {
             public void run() {
